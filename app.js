510f8cc0c250fbb6624f840d5d629500c86a9646
krzempoluch <krzempoluch@metoda.com.pl>
2014-11-05 16:09:43 +0100

    Poprawka

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
